4adfb5edf02f211434f7dae2be88dcded5dab3e0
ACER <juliusmanipol@gmail.com>
2024-02-18 16:35:17 +0800

    sample joni builder

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,6 +8,10 @@ class Home extends BaseController
     {
        $db= \Config\Database::connect();
        $builder = $db->table('authors');
+
+
+       $builder->select("posts.*, CONCAT(authors.first_name, ' ', authors.last_name) AS author");
+       $builder->join('posts', 'posts.author_id = authors.id');
        $query = $builder->get();
        $result = $query->getResult();
        return json_encode($result);
