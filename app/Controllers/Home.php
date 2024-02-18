bd743fc62002d6121aba544925d5c2a12c9d6831
ACER <juliusmanipol@gmail.com>
2024-02-18 23:28:10 +0800

    updated landing pag after login

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,15 +6,17 @@ class Home extends BaseController
 {
     public function index(): string
     {
-       $db= \Config\Database::connect();
-       $builder = $db->table('authors');
+    //    $db= \Config\Database::connect();
+    //    $builder = $db->table('authors');
 
 
-       $builder->select("posts.*, CONCAT(authors.first_name, ' ', authors.last_name) AS author");
-       $builder->join('posts', 'posts.author_id = authors.id');
-       $query = $builder->get();
-       $result = $query->getResult();
-       return json_encode($result);
+    //    $builder->select("posts.*, CONCAT(authors.first_name, ' ', authors.last_name) AS author");
+    //    $builder->join('posts', 'posts.author_id = authors.id');
+    //    $query = $builder->get();
+    //    $result = $query->getResult();
+    //    return json_encode($result);
+
+    return view('welcome_message');
 
     }
 }
