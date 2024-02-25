0495e0923b56c737e0e86d15a32f9a5898983954
ACER <juliusmanipol@gmail.com>
2024-02-25 22:05:16 +0800

    added user on sidebar

--- app/Views/templates/menu.php
+++ app/Views/templates/menu.php
@@ -26,8 +26,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="<?=base_url()?>public/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <!-- <img src="<?=base_url()?>public/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
+      <span class="brand-text font-weight-light">SPELS</span>
     </a>
 
     <!-- Sidebar -->
@@ -35,10 +35,12 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?=base_url()?>public/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <!-- <img src="<?=base_url()?>public/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> -->
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">
+            <?= auth()->user()->username ?? "Guest" ?>
+          </a>
         </div>
       </div>
 
